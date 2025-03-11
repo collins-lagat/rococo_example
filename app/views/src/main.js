461cb0f20ed7c4add8c218930b20a173790f224e
Collins Lagat <cklagat@gmail.com>
2025-03-11 23:07:39 +0300

    feat: setup quasar and pinia

--- app/views/src/main.js
+++ app/views/src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import { createPinia } from 'pinia'
+
 import './style.css'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+})
+app.use(pinia)
+
+app.mount('#app')
