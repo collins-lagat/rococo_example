8c20c0a9ae29a90e990ec2295533b5719e25c913
Collins Lagat <cklagat@gmail.com>
2025-03-11 22:39:53 +0300

    feat(wip): account resource

--- app/resources/account.py
+++ app/resources/account.py
@@ -1,11 +1,12 @@
+from flask import request
 from flask_restful import Resource
+
+from app.repositories.factory import RepositoryFactory
+from app.repositories.person import PersonRepository
 
 
 class Account(Resource):
     def get(self):
-        return {
-            "entity_id": "1",
-            "active": True,
-            "first_name": "John",
-            "last_name": "Doe",
-        }
+        person_repo = RepositoryFactory.get_repository(PersonRepository)
+        person = person_repo.get_many()[0]
+        return person.as_dict(convert_datetime_to_iso_string=True)
