2dadb90b1ecc0c7f5144583439bde07e2ea61585
Collins Lagat <cklagat@gmail.com>
2025-03-12 16:56:40 +0300

    feat: change name in toolbar

--- app/views/src/layouts/main.vue
+++ app/views/src/layouts/main.vue
@@ -3,7 +3,7 @@
     <q-header elevated class="bg-primary text-white">
       <q-toolbar inset>
         <q-breadcrumbs active-color="white" style="font-size: 16px">
-          <q-breadcrumbs-el label="Home" icon="home" />
+          <q-breadcrumbs-el label="App" icon="home" />
           <q-breadcrumbs-el :label="$route.meta.title" />
         </q-breadcrumbs>
       </q-toolbar>
