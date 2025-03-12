cdc5a6b8dfe3e9b0c17c123b0c07adea03eec7e5
Collins Lagat <cklagat@gmail.com>
2025-03-12 14:19:38 +0300

    feat: setup vue router

--- app/views/src/main.js
+++ app/views/src/main.js
@@ -1,15 +1,15 @@
-import { createApp } from 'vue'
-import { Quasar } from 'quasar'
-import { createPinia } from 'pinia'
-
-import './style.css'
-// Import icon libraries
-import '@quasar/extras/material-icons/material-icons.css'
-
-// Import Quasar css
-import 'quasar/src/css/index.sass'
-
-import App from './App.vue'
+import { createPinia } from 'pinia';
+import { Quasar } from 'quasar';
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from "vue-router";
+import './style.css';
+import '@quasar/extras/material-icons/material-icons.css';
+import 'quasar/src/css/index.sass';
+import App from './App.vue';
+import Index from './pages/index.vue';
+import NewPassword from './pages/new-password.vue';
+import SignIn from './pages/sign-in.vue';
+import SignUp from './pages/sign-up.vue';
 
 const pinia = createPinia()
 const app = createApp(App)
@@ -17,6 +17,32 @@ const app = createApp(App)
 app.use(Quasar, {
   plugins: {}, // import Quasar plugins and add here
 })
+
 app.use(pinia)
 
+const routes = [
+  {
+    path: '/',
+    component: Index,
+  },
+  {
+    path: '/new-password',
+    component: NewPassword,
+  },
+  {
+    path: '/sign-in',
+    component: SignIn,
+  },
+  {
+    path: '/sign-up',
+    component: SignUp,
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+app.use(router)
 app.mount('#app')
